971b7561a261d3f806ceab4bc88228a92ffb3247
AdamM <adam.m@liminalvr.com>
2021-07-08 19:59:27 +1000

    Added Pier model

---
+++ Assets/_ProjectFiles/Scripts/FixReflectionsInEditorOnly.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixReflectionsInEditorOnly : MonoBehaviour
+{
+    // This is just to fix the reflections in the editor when testing on QuestLink
+    public Renderer ReflectionSurface;
+    void Start()
+    {
+#if UNITY_EDITOR
+        StartCoroutine(LateStart());
+    #endif
+    }
+    IEnumerator LateStart()
+    {
+        yield return new WaitForSeconds(1);
+        var mat = ReflectionSurface.material;
+        mat.SetFloat("_Quest", 1);
+        mat.SetFloat("_OffsetEnabled", 1);
+    }
+}
