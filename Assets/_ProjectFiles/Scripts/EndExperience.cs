b94bb496091813828dca558dddaf8d867ae50524
AdamM <adam.m@liminalvr.com>
2021-07-09 01:09:02 +1000

    added timeline control and exit code

---
+++ Assets/_ProjectFiles/Scripts/EndExperience.cs
@@ -0,0 +1,38 @@
+﻿using Liminal.Core.Fader;
+using Liminal.SDK.Core;
+using Liminal.SDK.VR.Avatars;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndExperience : MonoBehaviour
+{
+    public CompoundScreenFader CompoundScreenFader;
+    public MeshScreenFader MeshScreenFader;
+
+    public void End()
+    {
+        StartCoroutine(EndGame());
+
+        IEnumerator EndGame()
+        {
+
+
+            var elapsedTime = 0f;
+            var fadeTime = 5f;
+            var startingVolume = AudioListener.volume;
+
+            ScreenFader.Instance.FadeToBlack(fadeTime);
+
+            while (elapsedTime < fadeTime)
+            {
+                elapsedTime += Time.deltaTime;
+                AudioListener.volume = Mathf.Lerp(startingVolume, 0f, elapsedTime / fadeTime);
+                yield return new WaitForEndOfFrame();
+            }
+
+            yield return ScreenFader.Instance.WaitUntilFadeComplete();
+            ExperienceApp.End();
+        }
+    }
+}
