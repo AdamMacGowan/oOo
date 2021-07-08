54a1b12e724ecf87c1e1adbb018bab2df83db901
AdamM <adam.m@liminalvr.com>
2021-07-08 20:49:49 +1000

    Optimising Geo

--- Assets/_ProjectFiles/Scripts/FixReflectionsInEditorOnly.cs
+++ Assets/_ProjectFiles/Scripts/FixReflectionsInEditorOnly.cs
@@ -14,7 +14,7 @@ public class FixReflectionsInEditorOnly : MonoBehaviour
     }
     IEnumerator LateStart()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.5f);
         var mat = ReflectionSurface.material;
         mat.SetFloat("_Quest", 1);
         mat.SetFloat("_OffsetEnabled", 1);
