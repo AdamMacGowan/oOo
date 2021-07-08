997c794640cb3e1e1a2f3255781fb0da1434b396
AdamM <adam.m@liminalvr.com>
2021-07-08 13:47:52 +1000

    ReflectionSetup

---
+++ Assets/_ProjectFiles/Scripts/RotateRings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float breathesPerMinute = 2f;
+    public float maxRotation = 45f;
+
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(0f, 0f, maxRotation * Mathf.Sin(Time.time / (30 / breathesPerMinute) ));
+    }
+}
